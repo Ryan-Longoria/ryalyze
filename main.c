abbafa4e5bac8075f4c8af12d51fb2e9c2103504
Ryan Longoria <89551302+Ryan-Longoria@users.noreply.github.com>
2022-07-13 09:38:06 -0500

    Commented out all OS besides Win32 and 64 to test

--- main.c
+++ main.c
@@ -9,11 +9,11 @@ int main (int argc, char* argv[])
 	#ifdef _WIN32
 		printf("You have a Windows operating system.");
 	#endif
-	/*
+	
 	#ifdef _WIN64
 		printf("You have a Windows operating system.");
 	#endif
-	
+	/*
 	#ifdef __APPLE__
 		printf("You have an Apple operating system.");
 	#endif
